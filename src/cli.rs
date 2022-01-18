4d53cb0eee5033eefa8db951ceebeaed184ed5ee
cgair <cgair@cgairdeMacBook-Pro.local>
2022-01-18 23:16:50 +0800

    Add command line parser

---
+++ src/cli.rs
@@ -0,0 +1,43 @@
+use structopt::StructOpt;
+use std::path::PathBuf;
+use crate::DEFAULT_CONF_PATH;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "ufs_cli", about = "ufs command line tool")]
+pub struct Opt {
+    #[structopt(subcommand)]
+    pub command: Command
+}
+
+#[derive(Debug, StructOpt)]
+pub enum Command {
+    /// Upload file
+    #[structopt(name = "upload")]
+    Upload(UploadOpt),
+
+    /// Start file server
+    #[structopt(name = "start")]
+    Start(StartOpt),
+}
+
+#[derive(Debug, StructOpt)]
+pub struct UploadOpt {
+    #[structopt(short, long = "ip-address")]
+    pub ip: String,
+    #[structopt(short, long)]
+    pub port: u32,
+    #[structopt(short = "f", long = "file-path")]
+    pub file: PathBuf,
+}
+
+#[derive(Debug, StructOpt)]
+pub struct StartOpt {
+    #[structopt(short, long, default_value = "127.0.0.1")]
+    pub host: String,
+    #[structopt(short, long = "ip-address", default_value = "[::]")]
+    pub ip: String,
+    #[structopt(short, long, default_value = "8080")]
+    pub port: u32,
+    #[structopt(short, long, default_value = "DEFAULT_CONF_PATH")]
+    pub config: String,
+}
