3299930a79edf7ede731c09123a1c85fc8356921
cgair <cgair@cgairdeMacBook-Pro.local>
2022-01-21 16:33:44 +0800

    1.Validate and return a directory path;2. Add [macro_use] to use macro inside crate

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,8 @@
+#![macro_use]
+use std::path::{Path, PathBuf};
 
 // Encountered a fatal error
 // Print error message and exit the current process
-#[macro_export]
 macro_rules! exit {
     ($($arg:tt)*) => {
         {   
@@ -10,4 +11,30 @@ macro_rules! exit {
             std::process::exit(1)
         }
     };
+}
+
+/// Validate and return a directory path.
+pub fn get_valid_dirpath<P: AsRef<Path>>(path: P) -> anyhow::Result<PathBuf>
+    where PathBuf: From<P>,
+{
+    match PathBuf::from(path) {
+        p if !p.exists() => exit!("path \"{:?}\" was not found or inaccessible", &p),
+        p if !p.is_dir() => exit!("path \"{:?}\" is not a valid directory", &p),
+        p => Ok(p)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::{Path, PathBuf};
+    #[test]
+    fn test_pathbuf() {
+        let path = PathBuf::from("not_exist.txt");
+        assert_eq!(path.exists(), false);
+        let path2: PathBuf = [r"config/", "default.toml"].iter().collect();
+        assert_eq!(path2.exists(), true);
+
+        assert_eq!(Path::new("SMLNODE/fs/").is_dir(), true);
+        assert_eq!(Path::new("a_file.txt").is_dir(), false);
+    }
 }
